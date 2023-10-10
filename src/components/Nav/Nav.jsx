6af01971fe9a8ea4859eb9b89f8ff23fb451081d
Paul HoangLong <paulhoanglong@gmail.com>
2023-10-10 11:23:39 -0500

    App bar made

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,45 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+import { useSelector } from "react-redux";
 
 function Nav() {
   const user = useSelector((store) => store.user);
 
   return (
     <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
+      <ul>
+        <li>
+          {" "}
+          <Link to="/home">
+            <h2 className="nav-title">Kanji For You</h2>
+          </Link>
+        </li>
+
         {/* If no user is logged in, show these links */}
         {!user.id && (
           // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
+          <li>
+            <Link className="navLink" to="/login">
+              Login / Register
+            </Link>
+          </li>
         )}
 
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
+            <li>
+              <Link className="navLink" to="/user">
+                Home
+              </Link>
+            </li>
 
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
+            <li>
+              <Link className="navLink" to="/collection">
+                Collection
+              </Link>
+            </li>
 
-            <LogOutButton className="navLink" />
+            <li>
+              <Link className="navLink" to="/Dictionary">
+                Dictionary
+              </Link>
+            </li>
+
+            <li>
+              <LogOutButton className="navLink" />
+            </li>
           </>
         )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
+        <li>
+          {" "}
+          <Link className="navLink" to="/about">
+            About
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
