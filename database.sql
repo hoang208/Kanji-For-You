c0e2aa10c2e5467aa2c20c0bc5b8845b1705ec2f
Paul HoangLong <paulhoanglong@gmail.com>
2023-10-11 13:54:55 -0500

    added post to kanji of the day

--- database.sql
+++ database.sql
@@ -3,6 +3,8 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+-- MUST SET DATABASE ENCODING TO UTF-8 OR SOMETHING THAT SUPPORTS JAPANESE LANGUAGE
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
@@ -16,12 +18,11 @@ CREATE TABLE "status" (
 
 CREATE TABLE "collection" (
   "id" SERIAL PRIMARY KEY,
-  "user_id" INT REFERENCES "user" NOT NULL DEFAULT 1,
-  "kanji" VARCHAR (80) NOT NULL,
-  "status_id" INT REFERENCES "status" NOT NULL,
+  "user_id" INT REFERENCES "user" NOT NULL,
+  "kanji" VARCHAR (80) UNIQUE NOT NULL,
+  "status_id" INT REFERENCES "status" NOT NULL　DEFAULT 1,
   "study_notes" VARCHAR (1000) DEFAULT 'No study note available'
 );
 
-INSERT INTO "status" ("status") VALUES ('Not Learned'),('Plan to Learn'),('Learning'),('Learned');
 
-INSERT INTO  "collection" ("kanji") VALUES ('一'）;
+INSERT INTO "status" ("status") VALUES ('Not Learned'),('Plan to Learn'),('Learning'),('Learned');
