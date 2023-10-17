d913ce4487abd7107d09b472b80a216732608c32
Paul HoangLong <paulhoanglong@gmail.com>
2023-10-17 15:08:04 -0500

    Added error pages for when a status have no kanji

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -173,17 +173,22 @@ export default function Collection() {
                 </tr>
               </thead>
               <tbody className="tableBody">
-                {filterItems.length ? ( 
+                {filteredItems.length ? (
                   <>
-                  {filteredItems.map((item) => (
-                  <CollectionTableItem
-                    key={item.id}
-                    kanji={item.kanji}
-                    status={item.status}
-                    notes={item.study_notes}
-                  />
-                ))}</>):(<tr><td>No Kanji availble</td></tr>)}
-               
+                    {filteredItems.map((item) => (
+                      <CollectionTableItem
+                        key={item.id}
+                        kanji={item.kanji}
+                        status={item.status}
+                        notes={item.study_notes}
+                      />
+                    ))}
+                  </>
+                ) : (
+                  <tr>
+                    <td colSpan="4">No Kanji  with this status available</td>
+                  </tr>
+                )}
               </tbody>
             </table>
           </div>
@@ -191,19 +196,28 @@ export default function Collection() {
       ) : (
         <>
           <div className="cardWrapper">
-            {filteredItems.slice(0, count).map((item) => (
-              <CollectionItem
-                key={item.id}
-                kanji={item.kanji}
-                status={item.status}
-              />
-            ))}
+            {filteredItems.length ? (
+              <>
+                {filteredItems.slice(0, count).map((item) => (
+                  <CollectionItem
+                    key={item.id}
+                    kanji={item.kanji}
+                    status={item.status}
+                  />
+                ))}
+              </>
+            ) : (
+              <div className="errorContainer">
+                <p>No Kanji  with this status available</p>
+              </div>
+            )}
           </div>
+          {filteredItems.length ? (
           <div className="search">
             <button className="button load" onClick={handleLoad}>
               Load More Kanji
             </button>
-          </div>
+          </div>) : ('')}
         </>
       )}
     </div>
